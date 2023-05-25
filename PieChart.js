aa772b2c811cbeea9cb3c70f9e5c7a0291248226
sanjana19991118 <69705801+sanjana19991118@users.noreply.github.com>
2023-05-25 06:01:18 +0530

    first commit

---
+++ PieChart.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Chart from 'react-apexcharts'
+
+function PieChart() {
+    return  (
+        <div className="">
+        <Chart 
+         type="pie"
+         
+
+        ></Chart>
+        </div>
+    )
+}
+
+export default PieChart
